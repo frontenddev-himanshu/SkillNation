558c0654db2f69d421c96e86d50f63b4b6afa1ed
frontenddev-himanshu <rajh5343@gmail.com>
2025-03-27 20:38:45 +0530

    add some js

--- script.js
+++ script.js
@@ -15,3 +15,44 @@ ham.addEventListener("click", () => {
     }
 
 })
+
+let navJump = document.querySelector(".navBar");
+
+window.addEventListener("scroll", function () {
+
+    if (this.window.pageYOffset > 100) {
+        navJump.classList.add("on");
+    }
+
+    else {
+        navJump.classList.remove("on")
+    }
+
+})
+
+
+// loader
+let randomTime = Math.floor(Math.random() * 3000) + 3000;
+console.log(randomTime);
+
+let loaderPage = document.querySelector('.loader');
+
+setTimeout(() => {
+
+    loaderPage.style.visibility = "hidden"
+
+}, randomTime);
+
+
+// cursor
+let cursor = document.querySelector("#cursor");
+let body = document.querySelector("body");
+
+window.addEventListener("mousemove", function (dets) {
+
+    gsap.to(cursor, {
+        x: dets.x,
+        y: dets.y
+    })
+
+})
